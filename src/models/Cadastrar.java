78db1051cc0a6de70e485f97e8d39c3d6a5a0444
Luiz Guilherme <luiz-234@hotmail.com>
2020-04-18 00:24:09 -0300

    Versão 1.4 (Finalizado) Novas Funcionalidades Para conferir veja o README

--- src/models/Cadastrar.java
+++ src/models/Cadastrar.java
@@ -6,10 +6,11 @@
 package models;
 
 import controllers.Jogador;
+import controllers.Jogar;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-//import java.sql.ResultSet;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -20,34 +21,43 @@ import java.sql.Statement;
 public class Cadastrar extends Jogador {
     
     public void recebeDados(controllers.Jogador jogador) throws SQLException, NoSuchAlgorithmException{
-        
-        if(!jogador.getSenha().equals(jogador.getConf_Senha())) {
-            //verifica se as variaveis senha1 e senha 2 sao iguais
-            System.out.println("SENHAS DIFERENTES");
-        }else if(jogador.getSenhaConvertida() <= 5){//verifica se a senha é menor ou igual que 7
-            System.out.println("Sua senha deve conter mais que 7 digitos");
-        }else{
-            Conexao con = new Conexao();
+              
+        Conexao con = new Conexao();
             
-            Statement st = con.conexao.createStatement();
-            MessageDigest senhaCriptografada=MessageDigest.getInstance("MD5");//criptografa a senha
+        Statement st = con.conexao.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM jogador WHERE nome_jogador = '"
+                        + jogador.getNome() +"'");//Select na tabela jogador
+        while(rs.next()){
+                String usuario = rs.getString("nome_jogador");
+                if(jogador.getNome().equals(usuario)){
+                    System.out.println("Nome de cadastro em uso!");
+                    Jogar cadastro = new Jogar();
+                    cadastro.menuJogo();
+                } else if(!jogador.getSenha().equals(jogador.getConf_Senha())){
+                    //verifica se as variaveis senha1 e senha 2 sao iguais
+                    System.out.println("SENHAS DIFERENTES");
+                    Jogar cadastro = new Jogar();
+                    cadastro.menuJogo();
+                } else if (jogador.getSenhaConvertida() <= 5) {
+                    //verifica se a senha é menor ou igual que 5
+                    System.out.println("Sua senha deve conter mais que 5 digitos");
+                    Jogar cadastro = new Jogar();
+                    cadastro.menuJogo();
+                } else {
+                    MessageDigest senhaCriptografada=MessageDigest.getInstance("MD5");//criptografa a senha
             
-            byte[] senhabytes = jogador.getSenha().getBytes();
-            senhaCriptografada.update(senhabytes,0,senhabytes.length);
-            BigInteger i = new BigInteger(1,senhaCriptografada.digest());
-            String jujuba = String.format("%1$032X", i);
+                    byte[] senhabytes = jogador.getSenha().getBytes();
+                    senhaCriptografada.update(senhabytes,0,senhabytes.length);
+                    BigInteger i = new BigInteger(1,senhaCriptografada.digest());
+                    String jujuba = String.format("%1$032X", i);
             
-            st.executeUpdate("insert into jogador "+// insere no banco
-                    "(nome_jogador,senha,vitorias,derrotas)" +
-                    " values ('" + jogador.getNome() + "', '" + jujuba + "',0,0)");
-            System.out.println("Cadastro realizado com sucesso");
-            Logar entrar = new Logar();
-            entrar.Autenticar(jogador);
-            //st.executeQuery("Select * from jogador");//Select na tabela jogador
-            //ResultSet rs = st.getResultSet();
-            //while(rs.next()) {
-            //System.out.println(rs.getString("nome_jogador"));//mostra os dados da tabela jogador no console
-            //}
+                    st.executeUpdate("insert into jogador "+// insere no banco
+                        "(nome_jogador,senha,vitorias,derrotas)" +
+                        " values ('" + jogador.getNome() + "', '" + jujuba + "',0,0)");
+                    System.out.println("Cadastro realizado com sucesso");
+                    Logar entrar = new Logar();
+                    entrar.Autenticar(jogador);
+                }
         }
     }
 }
