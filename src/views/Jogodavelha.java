0a5bfc4763e10570cd66dea13dbb2d3c5795a2a6
Saulo Junior <36682208+SauloJuniorsj@users.noreply.github.com>
2020-06-12 11:06:39 -0300

    Update Jogodavelha.java

--- src/views/Jogodavelha.java
+++ src/views/Jogodavelha.java
@@ -1,4 +1,3 @@
-/*finalizado*/
 package views;
 
 import controllers.Jogar;
@@ -6,17 +5,38 @@ import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import interfaceGame.PainelfrontalController;
 
-public class Jogodavelha {
+public class Jogodavelha extends Application{
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("painelfrontal.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+    }
     
     public static void main(String[] args) {
         
+        Jogar jogo = new Jogar();
+        
         try {
-            Jogar jogo = new Jogar();
+            launch(args);
             jogo.menuJogo();
-            
         } catch (SQLException | NoSuchAlgorithmException ex) {
             Logger.getLogger(Jogodavelha.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    
 }        
